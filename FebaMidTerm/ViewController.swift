9c19793bb7ade94d2e57b25d45b0933216ff6d9b
user234888 <user234888@NY551.local>
2023-11-01 20:28:31 -0400

    Quadratic Scene with function

--- FebaMidTerm/ViewController.swift
+++ FebaMidTerm/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var findCityButton: UIButton!
     @IBOutlet weak var cityImage: UIImageView!
     @IBOutlet weak var cityName: UITextField!
+    
+    @IBOutlet weak var varA: UITextField!
+    @IBOutlet weak var varB: UITextField!
+    @IBOutlet weak var varC: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -38,6 +43,38 @@ class ViewController: UIViewController {
                     cityImage.image = UIImage(named: "Canada")
                 }
             }    }
+    
+    
+    @IBAction func findResult(_ sender: Any) {
+        if let textA = varA.text, let textB = varB.text, let textC = varC.text,
+           let a = Double(textA), let b = Double(textB), let c = Double(textC) {
+
+            let discriminant = b * b - 4 * a * c
+
+            if a == 0 {
+                resultLabel.text = "The value you entered for A is invalid."
+            } else if discriminant < 0 {
+                resultLabel.text = "There are no results since the discriminant is less than zero."
+            } else if discriminant == 0 {
+                let x = -b / (2 * a)
+                resultLabel.text = "There is only one value for X: \(x)"
+            } else {
+                let x1 = (-b + sqrt(discriminant)) / (2 * a)
+                let x2 = (-b - sqrt(discriminant)) / (2 * a)
+                resultLabel.text = "There are two values for X: \(x1) and \(x2)"
+            }
+        } else {
+            if varA.text?.isEmpty == true {
+                resultLabel.text = "Enter a value for A to find X."
+            } else if varB.text?.isEmpty == true {
+                resultLabel.text = "The value you entered for B is invalid."
+            } else if varC.text?.isEmpty == true {
+                resultLabel.text = "The value you entered for C is invalid."
+            } else {
+                resultLabel.text = "Invalid input. Please enter valid numeric values."
+            }
+        }
+    }
     
 }
 
